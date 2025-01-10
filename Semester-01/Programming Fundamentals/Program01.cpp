02c23c48881209bb0bfe50aa784f1feaa36c768f
Raees <raeesulabideen@outlook.com>
2025-01-10 23:08:55 +0500

    removed vector and simplified the Program01

--- Semester-01/Programming Fundamentals/Program01.cpp
+++ Semester-01/Programming Fundamentals/Program01.cpp
@@ -1,30 +1,24 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 // Program written by Raees 
 
-// Function to generate an array of multiples of a given number
-vector<int> arrayOfMultiples(int num, int length) {
-    vector<int> multiples;
+// Function to print multiples of a given number
+void printMultiples(int num, int length) {
     for (int i = 1; i <= length; ++i) {
-        multiples.push_back(num * i); // Add the multiple to the vector
+        cout << num * i << " "; // Print the multiple
     }
-    return multiples;
+    cout << endl;
 }
 
 int main() {
     int num = 5; // Number to generate multiples of
     int length = 10; // Number of multiples to generate
-    vector<int> result = arrayOfMultiples(num, length); // Get the multiples
 
     // Print the multiples
     cout << "Multiples of " << num << " up to length " << length << " are: ";
-    for (int i : result) {
-        cout << i << " ";
-    }
-    cout << endl;
+    printMultiples(num, length);
 
     return 0;
 }
