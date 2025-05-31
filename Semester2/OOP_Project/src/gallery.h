0e350bbf18a20328fe50d95233fb4312632f87d6
Muhammad Raees Ul Abadien <179106248+raeesulabadien@users.noreply.github.com>
2025-05-31 13:56:51 +0500

    Update gallery.h

--- Semester2/OOP_Project/src/gallery.h
+++ Semester2/OOP_Project/src/gallery.h
@@ -2,32 +2,54 @@
 #define GALLERY_H
 
 #include "artwork.h"
+#include "user.h"
 #include <vector>
+#include <string>
 
 using namespace std;
 
+// Gallery class manages a collection of artworks and users
 class Gallery {
 private:
-    vector<Artwork> artworks;
-    string name;
+    vector<Artwork> artworks;     // List of artworks
+    vector<User> users;           // List of users
+    string name;                  // Gallery name
+    string currentUser;           // Currently logged in username
+    bool isUserAdmin;             // Is current user an admin?
 
 public:
-    // Simple constructor
+    // Constructor
     Gallery(string name);
     
-    // Basic operations
-    void addArtwork(Artwork artwork);
-    void showAll();
-    void searchByArtist(string artist);
-    void searchByCategory(string category);
+    // User management
+    bool registerUser(const string& username, const string& password, bool isAdmin = false);
+    bool login(const string& username, const string& password);
+    void logout();
+    bool isLoggedIn() const;
+    bool isAdmin() const;
+    string getCurrentUsername() const;
+    
+    // Artwork management
+    bool addArtwork(const Artwork& artwork);
+    void showAll() const;
+    void searchByArtist(const string& artist) const;
+    void searchByCategory(const string& category) const;
     Artwork* findById(int id);
+    bool removeArtwork(int id);
+    bool rateArtwork(int id, int rating);
+    
+    // Statistics
+    void showStatistics() const;
+    void showMostExpensiveArtwork() const;
+    void showMostPopularArtist() const;
     
     // File operations
-    void saveToFile();
+    bool saveToFile(const string& filename = "gallery_data.txt") const;
+    bool loadFromFile(const string& filename = "gallery_data.txt");
     
-    // Simple getters
-    int getCount();
-    string getName();
+    // Getters
+    int getCount() const;
+    string getName() const;
 };
 
 #endif
